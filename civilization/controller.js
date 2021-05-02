85d897149fd0c3438b207e7f1c1331938e7d9300
petrumamartin <64807853+petrumamartin@users.noreply.github.com>
2021-05-01 21:37:42 -0400

    Update controller.js

--- civilization/controller.js
+++ civilization/controller.js
@@ -1,33 +1,66 @@
+/*==========     Controller for web page     ==========*/
 $(document).ready(function(){
+/*==========     ?     ==========*/
 
-	var list = $("#old-list").html();
-	$("#new-list").append(list);
 
-	$("#new-list").css({"background-color":"rgba(200,10,150,0.5)"});
+	/*==========     global variables     ==========*/
+	var pages = [ "civilization.html", "communication.html", "education.html", "government.html", "orange.html", "green.html", "salmon.html", "purple.html" ];
+	var subjects = [ "Civilization", "Communication", "Education", "Government", "Orange", "Green", "Salmon", "Purple" ];
 
+	$("body").css({
+		"margin": "50px 100px",
+		"font-size": "1.3em"
+	});
+
+	/*==========     website title     ==========*/
+	var mainTitle = "<h1 id='main-title'>Civilization</h1>";
+	$("body").append(mainTitle);
+	$("#main-title").css({
+		"text-align": "center",
+		"font-family":"Footlight MT",
+		"font-size": "4em"
+	});
+
+	/*==========     web page title     ==========*/
+	var subject = "<h1 id='subject-title'></h1>";
+	$("body").append(subject);
+	for(var i = 0; i < pages.length; i++) {
+		if ($("title").text() === subjects[i])
+			$("h1#subject-title").html(subjects[i]);
+	}
+	$("h1#subject-title").css({
+		"text-align": "center",
+		"font-family": "Footlight MT"
+	})
+
+	/*==========     navigation menu     ==========*/
+	var navBar = "<nav></nav>";
+	$("body").append(navBar);
 	var menuBar = "<ul></ul>";
 	$("nav").append(menuBar);
-
-	var menu = [ "Communication", "Edit", "Refactor", "Format", "View", "News", "Help" ];
-	var links = [ "red.html", "yellow.html", "blue.html", "orange.html", "green.html", "salmon.html", "purple.html" ];
+	var menu = [ "Communication", "Education", "Government", "Orange", "Green", "Salmon", "Purple" ];
 	for(var i = 0; i < menu.length; i++){
-		$("nav ul").append("<a href=" + links[i] + "><li class='menu'>" + menu[i] + "</li></a>");
+		$("nav ul").append("<a href=" + pages[i + 1] + "><li class='menu'>" + menu[i] + "</li></a>");
 	}
-
 	$(".menu").css({
 		"display": "inline-block",
 		"padding": "5px",
 		"padding-right": "5px",
 		"padding-left": "15px",
 		"margin-right": "10px",
+		"margin-bottom": "5px",
 		"border": "solid black 2px",
-		"width": "80px"
+		"border-radius": "10px",
+		"width": "180px",
+		"background-color": "rgb(255, 255, 255)",
+		"text-align": "center", 
+		"font-size": "1.2em"
 	});
 
-
-
-
-	$("#return-link").html("<a href='../welcome.html'><p>Return</p></a>");//.css({"color":"yellow","text-decoration":"none"});
+	/*==========     return link     ==========*/
+	var returnHome = "<div id='return-link'></div>";
+	$("body").append(returnHome);
+	$("#return-link").html("<a href='../welcome.html'><p>Return</p></a>");
 	$("#return-link a").css({
 		"color":"white",
 		"text-decoration":"none",
