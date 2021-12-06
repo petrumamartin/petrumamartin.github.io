2c5a367df7b0d51e996d8ec580934c413da8b365
petrumamartin <64807853+petrumamartin@users.noreply.github.com>
2021-12-06 12:27:42 -0500

    Update molecular-geometry.js

--- science/chemistry/molecular-geometry.js
+++ science/chemistry/molecular-geometry.js
@@ -12,32 +12,74 @@ function draw() {
 	const canvas = document.getElementById('canvas');
 	const ctx = canvas.getContext('2d');
 
-	ctx.fillStyle = 'cornflowerblue';
-	ctx.beginPath();
-	ctx.arc(150, 150, 10, 0, Math.PI * 2, false);
-	ctx.fill();
-	ctx.closePath();
-	ctx.fillStyle = 'firebrick';
-	ctx.beginPath();
-	ctx.arc(100, 180, 10, 0, Math.PI * 2, false);
-	ctx.fill();
-	ctx.closePath();
-	ctx.beginPath();
-	ctx.arc(200, 180, 10, 0, Math.PI * 2, false);
-	ctx.fill();
-	ctx.closePath();
-
-	ctx.fillStyle = 'black';
-  ctx.lineWidth = '3';
-	ctx.beginPath();
-	ctx.moveTo(108, 175);
-	ctx.lineTo(142, 155);
-	ctx.stroke();
-	ctx.closePath();
-	ctx.beginPath();
-	ctx.moveTo(157, 156);
-	ctx.lineTo(195, 175);
-	ctx.stroke();
-	ctx.closePath();
-
+	linear(ctx);
+	tetrahedral(ctx);
+	trigonalBipyramidal(ctx);
 }
+function linear(ctx) {
+
+	canvasText(ctx, 'linear', new Point(60, 20), 'black');
+	canvasCircle(ctx, new Point(150, 50), 'cornflowerblue');
+	canvasCircle(ctx, new Point(100, 50), 'firebrick');
+	canvasCircle(ctx, new Point(200, 50), 'firebrick');
+	canvasLine(ctx, new Point(110, 50), new Point(140, 50), 1, 'black');
+	canvasLine(ctx, new Point(160, 50), new Point(190, 50), 1, 'black');
+}
+function tetrahedral(ctx) {
+
+	canvasCircle(ctx, new Point(150, 200), 'cornflowerblue');
+	canvasCircle(ctx, new Point(100, 230), 'firebrick');
+	canvasCircle(ctx, new Point(200, 230), 'firebrick');
+	canvasCircle(ctx, new Point(150, 140), 'firebrick');
+
+	canvasLine(ctx, new Point(150, 190), new Point(150, 150), 1, 'black');
+	canvasLine(ctx, new Point(108, 225), new Point(142, 205), 1, 'black');
+	canvasLine(ctx, new Point(157, 206), new Point(192, 225), 1, 'black');
+
+	canvasText(ctx, 'trigonal planar', new Point(60, 105), 'black');
+}
+
+function trigonalBipyramidal(ctx) {
+	canvasText(ctx, 'tetrahedral', new Point(60, 290), 'black');
+
+	canvasCircle(ctx, new Point(150, 375), 'cornflowerblue');
+	canvasCircle(ctx, new Point(150, 320), 'firebrick');
+
+	canvasLine(ctx, new Point(150, 365), new Point(150, 330), 1, 'black');
+}
+
+
+
+function canvasText(context, text, p1, colour) {
+	context.fillStyle = colour;
+	context.font = '20px serif';
+	context.fillText(text, p1.x, p1.y);
+	context.fill();
+}
+function canvasCircle(context, p1, colour) {
+	context.fillStyle = colour;
+	context.beginPath();
+	context.arc(p1.x, p1.y, 10, 0, Math.PI * 2);
+	context.fill();
+	context.closePath();
+}
+function canvasLine(context, p1, p2, w, colour) {
+	context.fillStyle = colour;
+	context.lineWidth = w;
+
+	context.beginPath();
+	context.moveTo(p1.x, p1.y);
+	context.lineTo(p2.x, p2.y);
+	context.stroke();
+	context.closePath();
+}
+class Point{
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+
+
+
