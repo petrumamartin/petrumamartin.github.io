ff2ce66165beac48e43c52fda9328c402f83966b
petrumamartin <64807853+petrumamartin@users.noreply.github.com>
2021-12-17 23:03:25 -0500

    Update molecular-geometry.js

--- science/chemistry/molecular-geometry.js
+++ science/chemistry/molecular-geometry.js
@@ -64,48 +64,6 @@ function trigonalBipyramidal() {
 	canvasCircle(new Point(110, 565), 9, 'firebrick');
 }
 
-function canvasText(text, p1, colour) {
-	context.fillStyle = colour;
-	context.font = '20px serif';
-	context.beginPath();
-	context.fillText(text, p1.x, p1.y);
-	context.fill();
-	context.closePath();
-}
-function canvasCircle(p1, r, colour) {
-	context.fillStyle = colour;
-	context.beginPath();
-	context.arc(p1.x, p1.y, r, 0, Math.PI * 2);
-	context.fill();
-	context.closePath();
-}
-function canvasLine(p1, p2, w, colour) {
-	context.fillStyle = colour;
-	context.lineWidth = w;
-
-	context.beginPath();
-	context.moveTo(p1.x, p1.y);
-	context.lineTo(p2.x, p2.y);
-	context.stroke();
-	context.closePath();
-}
-function canvasFillTriangle(a, b, c, colour) {
-	context.fillStyle = colour;
-	
-	context.beginPath();
-	context.moveTo(a.x, a.y);
-	context.lineTo(b.x, b.y);
-	context.lineTo(c.x, c.y);
-	context.fill();
-	context.closePath();
-}
-class Point{
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
 
 
 
