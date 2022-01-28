a545e835a0b20cc8601f8742145614eb6e90188c
petrumamartin <64807853+petrumamartin@users.noreply.github.com>
2022-01-27 19:53:52 -0500

    Create proteins.js

---
+++ science/biology/molecules/scripts/proteins.js
@@ -0,0 +1,68 @@
+/*
+author:   Peter A. Martin
+name:     proteins.js
+date:     Friday, November 19, 2021
+purpose:  program code for biology section
+
+*/
+var aminoacidName = [
+  "Histidine", "Isoleucine", "Leucine", "Lysine", "Methionine", "Phenylalanine", "Threonine", "Tryptophan", "Valine", "Arginine", "Cysteine", "Glutamine", "Glycine", "Proline", "Tyrosine", "Alanine", "Aspartic acid", "Asparagine", "Glutamic acid", "Serine", "Selenocysteine", "Pyrrolysine" 
+];
+var aminoacidSymbol = [
+  "His", "Ile", "Leu", "Lys", "Met", "Phe", "Thr", "Trp", "Val", "Arg", "Cys", "Gln", "Gly", "Pro", "Tyr", "Ala", "Asp", "Asp", "Glu", "Ser", "Sec", "Pyl"
+];
+
+var aminoacidEssentiality = [
+  "E", "E", "E", "E", "E", "E", "E", "E", "E", "C", "C", "C", "C", "C", "C", "N", "N", "N", "N", "N", "N", "N"
+];
+
+printProteins();
+function printProteins() {
+  var text = "";
+
+  text += "<ul>";
+  for(var i = 0; i < aminoacidName.length; i++)
+    text += "<li><span onclick=\"proteininfo(" + i + ")\">" + aminoacidSymbol[i] + " " + aminoacidEssentiality[i] + " " + aminoacidName[i] + "</span></li>";
+  text += "</ul>";
+
+  document.getElementById("proteins").innerHTML = text;
+};
+
+
+function proteininfo(n){
+  var symbol = aminoacidSymbol[n];
+  var name = aminoacidName[n];
+  var aminoacid = new AminoAcidType(symbol, name);
+  aminoacid.printAminoAcidType();
+  
+};
+function clearData(){
+  document.getElementById("protein-properties").innerHTML = "";
+};
+
+class AminoAcid {
+  constructor(s) {
+    this.symbol = s;
+  }
+
+}
+class AminoAcidType extends AminoAcid {
+  constructor(s, n) {
+    super(s);
+    this.name = n;
+  }
+
+  printAminoAcidType() {
+    document.getElementById("protein-properties").innerHTML = this.symbol + " " + this.name;
+  };  
+}
+
+
+
+
+
+
+
+
+
+
