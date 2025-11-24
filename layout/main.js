d19ca62f1d4941b3938914556fcd5d26dc2c8578
petrumamartin <64807853+petrumamartin@users.noreply.github.com>
2025-11-24 12:22:24 -0500

    Create main.js

---
+++ layout/main.js
@@ -0,0 +1,44 @@
+/*
+ File name:  main.js
+ Created:    Monday, November 24, 2025
+ Author:     Peter A. Martin
+ Purpose:    Functions for layout
+*/
+
+
+let navOpen, navClose, navLinks, navFlag;
+function nav() {
+ navOpen = document.querySelector("#open");
+ navClose = document.querySelector("#close");
+ navLinks = document.querySelector("#links");
+
+ navClose.style.visibility = "hidden";
+ navLinks.style.visibility = "hidden";
+ navFlag = false;
+
+ navOpen.addEventListener('click', ()=> {
+  if( !navFlag ) {
+   navOpen.style.visibility = "hidden";
+   navClose.style.visibility = "visible";
+   navLinks.style.visibility = "visible";
+   navFlag = true;
+  }
+ });
+
+ navClose.addEventListener('click', ()=>{
+  if( navFlag ) {
+   navOpen.style.visibility = "visible";
+   navClose.style.visibility = "hidden";
+   navLinks.style.visibility = "hidden";
+   navFlag = false;
+  }
+ });
+
+}
+
+
+
+
+
+
+
